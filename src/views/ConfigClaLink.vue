7dd9655c719522df57d1a14d8c3c75a60ef69b36
ouchengle <969707751@qq.com>
2020-12-15 17:11:21 +0800

    modify cla list

--- src/views/ConfigClaLink.vue
+++ src/views/ConfigClaLink.vue
@@ -218,8 +218,9 @@
                 this.$store.commit('setCorpFDName', formData.get('files').name);
                 let reader = new FileReader();
                 // reader.readAsDataURL(formData.get('files'));
-                reader.readAsBinaryString(formData.get('files'));;
+                reader.readAsBinaryString(formData.get('files'));
                 reader.onload = () => {
+                    console.log(reader.result);
                     this.$store.commit('setCorpFD', reader.result)
                 };
             },
